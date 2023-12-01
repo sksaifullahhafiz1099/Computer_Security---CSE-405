01d38b5c2a0ea2a27e2dc4710f7e4141f0902d3d
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2023-12-01 10:40:29 +0600

    subbytes

---
+++ main.py
@@ -0,0 +1,17 @@
+from aes import AES
+
+# Creating an instance of the Person class
+aes = AES()
+matrix = [
+    [0x01, 0x0A, 0x1E, 0x3F],
+    [0x5B, 0x4D, 0x8C, 0x72],
+    [0xA5, 0xE9, 0x2F, 0x6D],
+    [0xBF, 0x4A, 0x7C, 0xD8]
+]
+
+m=aes.substitute_bytes(matrix)
+for row in m:
+    for element in row:
+        print(hex(element))
+
+
