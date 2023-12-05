763f45787c84d38f0a7ef99367a99051cbdb3f42
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2023-12-05 17:26:51 +0600

    Combined have to debug

--- Offline-1/client.py
+++ Offline-1/client.py
@@ -22,14 +22,14 @@ def start_client():
     # Step 1: Client receives a custom object from the server
     server_data = client_socket.recv(1024)
     server_object = pickle.loads(server_data)
-    print(f"Received from server: {server_object}")
+    #print(f"Received from server: {server_object}")
 
     upper_limit = server_object.p + 1 + 2 * int(server_object.p**0.5)
     # Bob
     Kb = generate_prime_in_range(2, upper_limit)
     Kbpub = apply_double_and_add_method(server_object.G, Kb, server_object.p, server_object.a)
-    key = apply_double_and_add_method(server_object.Kapub, Kb, server_object.p, server_object.a)
-    print("key: ",key)
+    key_bob = apply_double_and_add_method(server_object.Kapub, Kb, server_object.p, server_object.a)
+    print("key: ",key_bob)
     # Custom processing logic on the client
     # Construct a new CustomObject with the necessary information
     client_data = CustomObject(
@@ -42,7 +42,7 @@ def start_client():
         n=1,  # Adjust n as needed
         data=[[[0]]]  # Adjust data as needed
     )
-    print(f"Processed data: {client_data}")
+    #print(f"Processed data: {client_data}")
 
     # Step 2: Client sends another custom object to the server
     client_socket.send(pickle.dumps(client_data))
@@ -50,7 +50,115 @@ def start_client():
     # Step 3: Client receives another custom object from the server
     server_response_data = client_socket.recv(1024)
     server_response_object = pickle.loads(server_response_data)
-    print(f"Received from server: {server_response_object}")
+    #print(f"Received from server: {server_response_object}")
+
+    #----------------------------------------------------
+    from aes import AES
+    from key import Key
+    from util import Util
+
+    def decrypt(message,keys):
+        for i in range(11):
+            if i == 0:
+                message = aes.add_round_key(message,keys[10-i]) 
+            if i <= 9 and i > 0:
+                message = aes.inv_shift_row(message)
+                message = aes.inv_substitute_bytes(message)
+                message = aes.add_round_key(message,keys[10-i])
+                message = aes.inv_mix_columns(message)  
+            if i == 10:
+                message = aes.inv_shift_row(message)
+                message = aes.inv_substitute_bytes(message) 
+                message = aes.add_round_key(message,keys[10-i])
+        return message 
+
+    util = Util()
+    aes = AES()
+    keyExpand = Key()
+
+    IV = [
+            [0x33, 0xf1, 0x04, 0x2b], 
+            [0x00, 0x12, 0xb3, 0x68],
+            [0xf5, 0xa0, 0x10, 0x3e],
+            [0xf1, 0xbb, 0x19, 0x87]
+        ]
+
+    """
+    #taking cipher massage and creating string chunk array
+    message = input("Enter Message to Decrypt: ")
+    padded_input = util.pad_input(message)
+    input_chunks = util.chunk_string(padded_input)
+    """
+    """
+    input_chunk_m = [
+                        [
+                            [0x7f, 0x67, 0xac, 0xf5],
+                            [0xc8, 0x6b, 0x44, 0x5b],
+                            [0xbe, 0x9d, 0x81, 0xc5],
+                            [0x26, 0x20, 0x29, 0x8]
+                        ]
+                    ]
+    """
+    cipher_blocks = server_response_object.data
+    input_chunk_m = cipher_blocks
+
+    #taking key and creating chunk array(only first one will be used)
+    #user_key =input("Enter a Key: ")
+    user_key = str(key_bob[0])
+    padded_key = util.pad_input(user_key)
+    key_chunks = util.chunk_string(padded_key)
+
+    key = util.string_to_matrix(key_chunks[0])
+
+    keys = [[[0] * 4 for _ in range(4)] for _ in range(11)]
+    keys[0] = key 
+    for i in range(10):
+        print
+        key = keyExpand.expand(key, i)
+        keys[i+1] = key
+
+    """
+    encrypted_message = [[0x29, 0xc3, 0x50, 0x5f],
+        [0x57, 0x14, 0x20, 0xf6],
+        [0x40, 0x22, 0x99, 0xb3],
+        [0x1a, 0x2, 0xd7, 0x3a]]
+    encrypted_message = list(map(list, zip(*encrypted_message)))
+    encrypted_message = decrypt(encrypted_message, keys)
+    encrypted_message = list(map(list, zip(*encrypted_message)))
+    print(util.matrix_to_string(encrypted_message))
+    """
+
+
+
+    plain_blocks = [[[0] * 4 for _ in range(4)] for _ in range(len(input_chunk_m))]
+    #print(plain_blocks)
+    #print(input_chunk_m[0])
+    #print(util.matrix_to_string(input_chunk_m[0]))
+    for i in range(len(input_chunk_m)):
+        print(i)
+        P = input_chunk_m[i]
+        P = list(map(list, zip(*P)))
+        #util.print_mat(P)
+        if i == 0:
+            plain_blocks[0] = util.xor(decrypt(P,keys),IV)
+            plain_blocks[0] = list(map(list, zip(*plain_blocks[0])))
+            #print(util.matrix_to_string(plain_blocks[0]))
+        else:
+            #temp_mat = list(map(list, zip(*input_chunk_m[i-1])))
+            temp_mat = input_chunk_m[i-1]
+            plain_blocks[i] = util.xor(decrypt(P,keys),temp_mat)
+            plain_blocks[i] = list(map(list, zip(*plain_blocks[i])))
+
+    #print(util.matrix_to_string(plain_blocks[0]))
+    #util.print_mat(plain_blocks[0])
+    #print(plain_blocks)
+    plain_string = util.matrix_list_to_string(plain_blocks)
+    print(plain_string)
+    print("done")
+    #util.print_mat(message)
+    #message = list(map(list, zip(*message)))
+    #print(util.matrix_to_string(message))
+    #----------------------------------------------------
 
     client_socket.close()
 
--- Offline-1/server.py
+++ Offline-1/server.py
@@ -46,13 +46,96 @@ def start_server():
         # Step 2: Server receives a custom object from the client
         received_data = client_socket.recv(1024)
         client_object = pickle.loads(received_data)
-        print(f"Received from client: {client_object.Kapub}")
-        key =  apply_double_and_add_method(client_object.Kbpub,Ka,p,a)
-        print("key: ",key)
+        #print(f"Received from client: {client_object.Kapub}")
+        key_alice =  apply_double_and_add_method(client_object.Kbpub,Ka,p,a)
+        print("key: ",key_alice)
         #key =  apply_double_and_add_method(client_object.Kbpub,Ka,p,a) 
         # Custom processing logic on the server
+        #---------------------------------------
+
+        from aes import AES
+        from key import Key
+        from util import Util
+
+        def encrypt(message,key):
+            for i in range(11):
+                if i == 0:
+                    message = aes.add_round_key(message,key) 
+                if i <= 9 and i > 0:
+                    message = aes.substitute_bytes(message)
+                    message = aes.shift_row(message)
+                    message = aes.mix_columns(message)  
+                    message = aes.add_round_key(message,key)
+                if i == 10:
+                    message = aes.substitute_bytes(message) 
+                    message = aes.shift_row(message)
+                    message = aes.add_round_key(message,key) 
+                if i < 10:
+                    key = keyExpand.expand(key,i)
+            return message
+
+        util = Util()
+        aes = AES()
+        keyExpand = Key()
+
+        IV = [
+                [0x33, 0xf1, 0x04, 0x2b], 
+                [0x00, 0x12, 0xb3, 0x68],
+                [0xf5, 0xa0, 0x10, 0x3e],
+                [0xf1, 0xbb, 0x19, 0x87]
+            ]
+
+        #taking massage and creating string chunk array
+        message = input("Enter something: ")
+        padded_input = util.pad_input(message)
+        input_chunks = util.chunk_string(padded_input)
+
+        #taking key and creating chunk array(only first one will be used)
+        #user_key =input("Enter a Key: ")
+        user_key = key_alice
+        padded_key = util.pad_input(user_key)
+        key_chunks = util.chunk_string(padded_key)
+
+        #only the first chunk is taken as the key matrix
+        key = util.string_to_matrix(key_chunks[0])
+
+        #-----------------------------------------------
+        """
+        message = util.string_to_matrix(input_chunks[0])
+        plain_message = list(map(list, zip(*message)))
+        encrypted_message = encrypt(plain_message,key)
+        encrypted_message = list(map(list, zip(*encrypted_message)))
+        util.print_mat(encrypted_message)
+        print(util.matrix_to_string(encrypted_message))
+        """
+        print(input_chunks)
+
+        cipher_blocks = [[[0] * 4 for _ in range(4)] for _ in range(len(input_chunks))]
+        for i in range(len(input_chunks)):
+            P = util.string_to_matrix(input_chunks[i])
+            P = list(map(list, zip(*P)))
+            #util.print_mat(P)
+            if i == 0:
+                cipher_blocks[0] = encrypt(util.xor(IV,P),key)
+                cipher_blocks[0] = list(map(list, zip(*cipher_blocks[0])))
+                print(util.print_mat(cipher_blocks[0]))
+            else:
+                cipher_blocks[i] = encrypt(util.xor(cipher_blocks[i-1],P),key)
+                cipher_blocks[i] = list(map(list, zip(*cipher_blocks[i])))
+
+        #printing cipher strings        
+        #cipher_string = util.matrix_list_to_string_inv(cipher_blocks)
+        #print(cipher_string)
+        #util.print_mat(cipher_blocks[0])
+
+        for element in cipher_blocks:
+            util.print_mat(element)
+
+        
+        client_object.data = cipher_blocks
+        #---------------------------------------
         processed_data = client_object
-        print(f"Processed data: {processed_data.Kapub}")
+        #print(f"Processed data: {processed_data.Kapub}")
 
         # Step 3: Server sends another custom object to the client
         server_response = CustomObject(processed_data)
